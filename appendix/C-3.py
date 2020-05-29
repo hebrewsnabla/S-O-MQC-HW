0299a16fe3ade3f9e045ea79bf88199d816aaba8
hebrewsnabla <wangshirongnju@gmail.com>
2020-05-29 13:44:19 +0800

    update C-3.py

--- appendix/C-3.py
+++ appendix/C-3.py
@@ -29,41 +29,62 @@ for i in [0,1,2]:
 print(X,bar(X),E,E.min())
 
 def update(X,E,h,Xs):
-    #print("%f,%f -> %f,%f"%(X[h][0],X[h][1],Xs[0],Xs[1]))
+    print("update %f,%f -> %f,%f"%(X[h][0],X[h][1],Xs[0],Xs[1]))
     X[h] = Xs
     newX = X
-    E[h] = Es
+    E[h] = getE(Xs)
     newE = E
     return newX, newE
 
+def findh(E):
+    Emax = -100.0
+    h = -1
+    for i in range(len(E)):
+        if E[i] > Emax:
+            h = i
+            Emax = E[i]
+    return h, Emax
+def conv(dx):
+    thresh = 0.002
+    cv = abs(dx[0]) < thresh and abs(dx[1]) < thresh
+    return cv
+
+cyc = 1
 while(True):
     oldX = copy.copy(X)
     oldE = copy.copy(E)
     #print(oldX, oldE)
-    h=0
-    while(h<3):
-        Xs = (1+A)*bar(X) - A*X[h]
-        Es = getE(Xs)
-        if E.min() < Es < E[h]:
-            X,E = update(X,E,h, Xs)
-        elif Es < E.min():
-            Xss = v*Xs + (1-v)*bar(X)
-            Ess = getE(Xss)
-            if Ess < E.min():
-                X,E = update(X,E,h,Xss)
-            else:
-                X,E = update(X,E,h,Xs)
+    #h=0
+    print("Cycle %d"%cyc)
+    h, Emax = findh(E)
+    #print(h, Emax)
+    #while(h<3):
+    Xs = (1+A)*bar(X) - A*X[h]
+    Es = getE(Xs)
+    if E.min() < Es < E[h]:
+        X,E = update(X,E,h, Xs)
+    elif Es < E.min():
+        Xss = v*Xs + (1-v)*bar(X)
+        Ess = getE(Xss)
+        if Ess < E.min():
+            X,E = update(X,E,h,Xss)
         else:
-            Xss = B*X[h] + (1-B)*bar(X)
-            Ess = getE(Xss)
-            if Ess > E[h]:
-                h += 1
-            else:
-                X,E = update(X,E,h,Xss)
-        #print(X,bar(X),E,E.min())
-    #print(oldX, '->', X)
-    print(oldE, '->', E)
-    if np.linalg.norm(E - oldE) < 0.002:
+            X,E = update(X,E,h,Xs)
+    else:
+        Xss = B*X[h] + (1-B)*bar(X)
+        Ess = getE(Xss)
+        if Ess > E[h]:
+            print("conv failed")
+            break
+        else:
+            X,E = update(X,E,h,Xss)
+    #h += 1
+    #print(X,bar(X),E,E.min())
+    print('bar(X):', bar(oldX), '->', bar(X))
+    print('E     :', oldE, '->', E)
+    dX = bar(X) - bar(oldX)
+    if conv(dX):
         break
-
+    else:
+        cyc += 1
 
