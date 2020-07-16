80d30ae6e8c649ad01942a18d26495841177651f
hebrewsnabla <wsr@smail.nju.edu.cn>
2020-07-16 14:16:06 +0800

    add release.sh

---
+++ release.sh
@@ -0,0 +1,12 @@
+ver=$1
+name=SzaboSolutions
+mkdir ../$name-$ver
+for i in {1..7}
+do
+  cp chap$i/chap$i.pdf ../$name-$ver
+done
+cp appendix/appendix.pdf ../$name-$ver
+
+tar czvf ../$name-$ver.tar.gz ../$name-$ver
+
+
