c104026f287015155ee954cf50e06810ae68f3be
hebrewsnabla <wsr@smail.nju.edu.cn>
2020-08-07 21:51:20 +0800

    refine 5-12

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ done
 cp appendix/appendix.pdf $targ
 
 mkdir $targ/codes
-for j in chap1/1-1.nb "chap4/4-11,12.nb" chap5/5-7.nb chap5/5-9+.nb appendix/C-3.py appendix/C-4.py
+for j in chap1/1-1.nb chap1/1-3.nb "chap4/4-11,12.nb" "chap4/4-14,15.nb" chap5/5-7.nb chap5/5-9+.nb chap5/5-12.nb chap5/5-16+.nb chap6/6-6.nb appendix/C-3.py appendix/C-4.py
 do
   cp $j $targ/codes
 done
