c5958b0642daa70b249be6fd6dc40637718a334e
hebrewsnabla <srwang20@fudan.edu.cn>
2021-05-01 21:14:19 +0800

    update release.sh

--- release.sh
+++ release.sh
@@ -1,14 +1,15 @@
 ver=$1
 name=SzaboSolutions
 targ=release/$name-$ver
-mkdir $targ
+mkdir -p release
+mkdir -p $targ
 for i in {1..7}
 do
   cp chap$i/chap$i.pdf $targ
 done
 cp appendix/appendix.pdf $targ
 
-mkdir $targ/codes
+mkdir -p $targ/codes
 for j in chap1/1-1.nb chap1/1-3.nb "chap4/4-11,12.nb" "chap4/4-14,15.nb" chap5/5-7.nb chap5/5-9+.nb chap5/5-12.nb chap5/5-16+.nb chap6/6-6.nb appendix/C-3.py appendix/C-4.py
 do
   cp $j $targ/codes
