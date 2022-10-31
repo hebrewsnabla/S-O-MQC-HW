cad243990d706dc03efa356af9467ef53d50d057
jeanwsr <srwang20@fudan.edu.cn>
2022-10-31 15:51:45 +0800

    add ci

--- release.sh
+++ release.sh
@@ -5,9 +5,15 @@ mkdir -p release
 mkdir -p $targ
 for i in {1..7}
 do
-  cp chap$i/chap$i.pdf $targ
+  cd chap$i
+  latexmk -xelatex  -halt-on-error -shell-escape chap$i
+  cp chap$i.pdf ../$targ
+  cd ..
 done
-cp appendix/appendix.pdf $targ
+cd appendix
+latexmk -xelatex  -halt-on-error -shell-escape appendix
+cp appendix.pdf ../$targ
+cd ..
 
 mkdir -p $targ/codes
 for j in chap1/1-1.nb chap1/1-3.nb "chap4/4-11,12.nb" "chap4/4-14,15.nb" chap5/5-7.nb chap5/5-9+.nb chap5/5-12.nb chap5/5-16+.nb chap6/6-6.nb appendix/C-3.py appendix/C-4.py
@@ -15,6 +21,6 @@ do
   cp $j $targ/codes
 done
 
-tar czvf $targ.tar.gz $targ
+#tar czvf $targ.tar.gz $targ
 
 
